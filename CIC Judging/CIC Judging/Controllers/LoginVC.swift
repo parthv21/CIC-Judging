b51c57886a5b646ded682725f40272be99f7da68
Parth Tamane <parthv21@gmail.com>
2020-02-27 17:38:05 -0500

    initial app setup

---
+++ CIC Judging/CIC Judging/Controllers/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CIC Judging
+//
+//  Created by Parth Tamane on 27/02/20.
+//  Copyright © 2020 Parth Tamane. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    /**
+     Initial setup of LoginVC.
+     */
+    private func setUpView() {
+        view.backgroundColor = .white
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
